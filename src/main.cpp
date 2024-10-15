5a0c047932cc2f207b598fed7336efed66b08e42
Aldowo <A22310381@live.ceti.mx>
2024-10-15 00:21:47 +0000

    Clase 1

---
+++ src/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main (){
+
+string si;
+
+    cout<<"Angel me la saborea?"<<endl;
+    cin>>si;
+  //std::cout<<"Angel me la chupa"<<std::endl;
+
+}
